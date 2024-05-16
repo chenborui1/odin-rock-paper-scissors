3be5d530a400934103d9e574187707ac72dab499
Po-jui <chenborui1@gmail.com>
2024-05-16 17:30:24 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,35 +1,36 @@
 function playGame() {
-    let humanScore = 0;
-    let machineScore = 0;
-  
+  let humanScore = 0;
+  let machineScore = 0;
 
-  
-    for (let i = 0; i < 5; i++) {
-        const humanChoice = getHumanChoice();
-        const machineChoice = getComputerChoice();
-        let result = playRound(humanChoice, machineChoice)
-        console.log(result)
-        if (result.includes("You Win!")) {
-            humanScore++
-        }
-        else if (result.includes("You Lose!")) {
-            machineScore++
-        }
-        else {
-            humanScore++
-            machineScore++
-        }
+  for (let i = 0; i < 5; i++) {
+    const humanChoice = getHumanChoice();
+    const machineChoice = getComputerChoice();
+    let result = playRound(humanChoice, machineChoice);
+    console.log(result);
+    if (result.includes("You Win!")) {
+      humanScore++;
+    } else if (result.includes("You Lose!")) {
+      machineScore++;
+    } else {
+      humanScore++;
+      machineScore++;
     }
- 
-    if (humanScore > machineScore) {
-        return "Human wins with a score of " + humanScore;
-    }
-    else if (humanScore < machineScore){
-        return "Machine wins with a score of " + machineScore;
-    }
-    return "Tie Game! " + "Human Score: " + humanScore + " Machine Score " + machineScore; 
   }
-  
+
+  if (humanScore > machineScore) {
+    return "Human wins with a score of " + humanScore;
+  } else if (humanScore < machineScore) {
+    return "Machine wins with a score of " + machineScore;
+  }
+  return (
+    "Tie Game! " +
+    "Human Score: " +
+    humanScore +
+    " Machine Score " +
+    machineScore
+  );
+}
+
 function getComputerChoice() {
   random = Math.random() * 3;
   if (random < 1) {
@@ -56,13 +57,25 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, machineChoice) {
-    const outcomes = {
-        Rock: { Rock: "Tie! Rock ties Rock", Paper: "You Lose! Paper beats Rock", Scissors: "You Win! Rock beats Scissors" },
-        Paper: { Rock: "You Win! Paper beats Rock", Paper: "Tie! Paper ties Paper", Scissors: "You Lose! Scissors beats Paper" },
-        Scissors: { Rock: "You Lose! Rock beats Scissors", Paper: "You Win! Scissors beats Paper", Scissors: "Tie! Scissors ties Scissors" }
-      };
-    
-      return outcomes[humanChoice][machineChoice] || "Invalid input";
+  const outcomes = {
+    Rock: {
+      Rock: "Tie! Rock ties Rock",
+      Paper: "You Lose! Paper beats Rock",
+      Scissors: "You Win! Rock beats Scissors",
+    },
+    Paper: {
+      Rock: "You Win! Paper beats Rock",
+      Paper: "Tie! Paper ties Paper",
+      Scissors: "You Lose! Scissors beats Paper",
+    },
+    Scissors: {
+      Rock: "You Lose! Rock beats Scissors",
+      Paper: "You Win! Scissors beats Paper",
+      Scissors: "Tie! Scissors ties Scissors",
+    },
+  };
+
+  return outcomes[humanChoice][machineChoice] || "Invalid input";
 }
 
-console.log(playGame())
+console.log(playGame());
